2c7b322907e69bfdc251af5688d2f03c3109e7cf
Chris <chris171819@googlemail.com>
2020-03-07 13:42:55 +0000

    3.10

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is Simply a test", "https://image.shutterstock.com/image-photo/fresh-tasty-burger-isolated-on-260nw-705104968.jpg")
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  addNewRecipe(){
+    this.recipes.push(this.recipes[0]);
+  }
 }
